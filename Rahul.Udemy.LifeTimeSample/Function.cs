111dabae23d04bfbd94a3733899354744c44a3ed
Rahul Nath <rahulpnath@gmail.com>
2022-06-02 05:00:44 +1000

    Lambbda LifeTime Demo

---
+++ Rahul.Udemy.LifeTimeSample/Function.cs
@@ -0,0 +1,29 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Rahul.Udemy.LifeTimeSample;
+
+public class Function
+{
+    private int Count;
+
+    public Function()
+    {
+        Count = 0;
+    }
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context)
+    {
+        Count++;
+        Thread.Sleep(5000);
+        return $"{input} - {Count}".ToUpper();
+    }
+}
